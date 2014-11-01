cc88997c56f1489b4c46e37f2d46b6c32b77e058
Plamen <stanev.plamen@gmail.com>
2014-11-01 14:46:33 +0200

    Sync and commit
    start view image add
    high scores view add

---
+++ ScoresController.h
@@ -0,0 +1,13 @@
+//
+//  ScoresController.h
+//  FlappyNinja
+//
+//  Created by Ivelina Vassileva on 01.11.14.
+//  Copyright (c) 2014 Test Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScoresController : UIViewController
+
+@end
---
+++ ScoresController.m
@@ -0,0 +1,38 @@
+//
+//  ScoresController.m
+//  FlappyNinja
+//
+//  Created by Ivelina Vassileva on 01.11.14.
+//  Copyright (c) 2014 Test Test. All rights reserved.
+//
+
+#import "ScoresController.h"
+
+@interface ScoresController ()
+
+@end
+
+@implementation ScoresController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
